f2da825d7be6b53e6fd874e549161ab03e0d7c53
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-07-27 14:57:10 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (88.62%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -1,17 +1,18 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        int maxcount=0;
+        int MaxCount=0;
         for(int i=0;i<sentences.length;i++){
-            int count=1;
-            for(int j=0;j<sentences[i].length();j++){
-                char ch=sentences[i].charAt(j);
-                if(ch == ' '){
-                    count++;
-                }
-            }
-            maxcount=Math.max(maxcount,count);
-            
+            MaxCount=Math.max(MaxCount,count(sentences[i]));
         }
-        return maxcount;
+        return MaxCount;
+    }
+    public int count(String s){
+        int count=1;
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)==' '){
+                count++;
+            }
+        }
+        return count;
     }
 }

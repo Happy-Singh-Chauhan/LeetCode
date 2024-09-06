db97b2f9f62c967b27c832d512ec36cba8847dd8
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-09-06 23:08:40 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (94.82%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,27 +1,5 @@
 class Solution {
-    public boolean check(int n,int num){
-        String str="";
-        while(n>0){
-            str+=n%num;
-            n/=num;
-        }
-        int start=0;
-        int end=str.length()-1;
-        while(start<end){
-            if(str.charAt(start) != str.charAt(end)){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
     public boolean isStrictlyPalindromic(int n) {
-        for(int i=2;i<=n-2;i++){
-            if(!check(n,i)){
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 }

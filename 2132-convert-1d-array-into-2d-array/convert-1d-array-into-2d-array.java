3be93d8f13938f4baea0cfef342fc0228d6515c4
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-09-07 22:03:31 +0530

    Time: 5 ms (42.32%) | Memory: 55.5 MB (59.53%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -4,24 +4,13 @@ class Solution {
         if(m*n != original.length){
             return new int[][]{};
         }
-        if(m == 1 ){
-            for(int i=0;i<n;i++){
-                arr[0][i]=original[i];
-            }
-        }
-        else if(n == 1){
-            for(int i=0;i<m;i++){
-                arr[i][0]=original[i];
-            }
-        }
-        else{
+        
         int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 arr[i][j]=original[ans];
                 ans++;
             }
-        }
         }
         return arr;
     }

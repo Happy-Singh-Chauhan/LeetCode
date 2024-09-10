82f817d44dbdc7abe2cb11effd594800ba8b7d84
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-09-10 20:36:54 +0530

    Time: 4 ms (61.04%) | Memory: 45.4 MB (54.13%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,11 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length == 1){
-            return nums[0];
-        }
-        if(nums.length<4){
-            return -1;
-        }
+        
         Arrays.sort(nums);
         int i=0;
         while(i<nums.length-1){

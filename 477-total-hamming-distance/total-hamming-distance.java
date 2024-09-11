288b9ffeeb0fc67038586be973c76e68a81173f7
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-09-11 10:54:59 +0530

    Time: 1783 ms (22.69%) | Memory: 44.8 MB (77.62%) - LeetSync

---
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                ans+=Integer.bitCount(nums[i]^nums[j]);
+            }
+        }
+        return ans;
+    }
+}

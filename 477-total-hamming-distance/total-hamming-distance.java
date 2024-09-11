611d3ccfc42b61d061764f24c9852abbb551b395
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-09-11 11:37:48 +0530

    Time: 3 ms (100.00%) | Memory: 45 MB (46.01%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.java
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -1,11 +1,15 @@
+// For each bit position 1-32 in a 32-bit integer, we count the number of integers in the array which have that bit set. 
+// Then, if there are n integers in the array and k of them have a particular bit set and (n-k) do not, 
+// then that bit contributes k*(n-k) hamming distance to the total.
 class Solution {
     public int totalHammingDistance(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                ans+=Integer.bitCount(nums[i]^nums[j]);
-            }
+        int total = 0, n = nums.length;
+        for (int j = 0; j < 32; j++) {
+            int bitCount = 0;
+            for (int i = 0; i < n; i++)
+                bitCount += (nums[i] >> j) & 1;
+            total += bitCount * (n - bitCount);
         }
-        return ans;
+        return total;
     }
 }

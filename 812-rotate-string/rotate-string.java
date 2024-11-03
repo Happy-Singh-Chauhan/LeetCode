881bf28892869da750a7537b6d5eb56bf6173731
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-11-03 22:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (15.40%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,12 +3,9 @@ class Solution {
         if(s.length() > goal.length()){
             return false;
         }
-        String str=s;
-        for(int i=0;i<s.length();i++){
-            str+=s.charAt(i);
-            if(str.contains(goal)){
-                return true;
-            }
+        String str=s+s;
+        if(str.contains(goal)){
+            return true;
         }
         return false;
     }

f8b2c070d103a3ac633bb2b06a71250b6045967e
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-11-19 00:08:08 +0530

    Time: 2 ms (65.76%) | Memory: 44.3 MB (56.60%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,23 +14,29 @@
  * }
  */
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0){
+    public TreeNode helper(int[] preorder, int[] inorder,int left,int right,HashMap<Integer,Integer> map,int[]index){
+        if(left > right){
             return null;
         }
-        int r=preorder[0];
-        int index=0;
-        for(int i=0;i<inorder.length;i++){
-            if(inorder[i] == r){
-                index=i;
-            }
+        int current=preorder[index[0]];
+        index[0]++;
+        TreeNode node=new TreeNode(current);
+        if(left == right){
+            return node;
         }
-        TreeNode node=new TreeNode(r);
+        int inorderIndex=map.get(current);
 
-        node.left=buildTree(Arrays.copyOfRange(preorder,1,index+1),Arrays.copyOfRange(inorder,0,index));
-
-        node.right=buildTree(Arrays.copyOfRange(preorder,index+1,preorder.length),Arrays.copyOfRange(inorder,index+1,inorder.length));
+        node.left=helper(preorder,inorder,left,inorderIndex-1,map,index);
+        node.right=helper(preorder,inorder,inorderIndex+1,right,map,index);
 
         return node;
+    }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int[] index={0};
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return helper(preorder,inorder,0,preorder.length-1,map,index);
     }
 }

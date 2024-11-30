1a623a11fbc53ab453dd604deda8d4df0cb12a56
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-12-01 01:38:43 +0530

    Time: 62 ms (13.45%) | Memory: 74.1 MB (26.62%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        HashSet<String> set=new HashSet<>();
+        int count=0;
+        for(String s:bannedWords){
+            set.add(s);
+        }
+        for(String str:message){
+            if(set.contains(str)){
+                count++;
+            }
+        }
+        if(count >= 2){
+            return true;
+        }
+        return false;
+    }
+}

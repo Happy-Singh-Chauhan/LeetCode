a46f056d26e1edeb23128f57495f6d2a3d954e92
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2024-12-28 00:22:34 +0530

    Time: 4 ms (89.46%) | Memory: 53.9 MB (16.76%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.java
@@ -1,15 +1,14 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         int n=values.length;
-        int[] maxScoreLeft=new int[n];
         int maxScore=0;
-        maxScoreLeft[0]=values[0];
-
+        int maxScoreLeft=values[0];
+        
         for(int i=1;i<n;i++){
             int currentScore=values[i]-i;
-            maxScore=Math.max(maxScore,currentScore+maxScoreLeft[i-1]);
+            maxScore=Math.max(maxScore,currentScore+maxScoreLeft);
             int current=values[i]+i;
-            maxScoreLeft[i]=Math.max(maxScoreLeft[i-1],current);
+            maxScoreLeft=Math.max(maxScoreLeft,current);
         }
         return maxScore;
     }

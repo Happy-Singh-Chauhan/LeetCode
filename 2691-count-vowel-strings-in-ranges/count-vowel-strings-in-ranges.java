6d33be66c139055adf15481f23fe869e98e50335
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-02 22:34:40 +0530

    Time: 6 ms (74.03%) | Memory: 85.5 MB (92.48%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,20 +1,36 @@
 class Solution {
-    public int[] vowelStrings(String[] words, int[][] queries) {
-        HashSet<Character> set=new HashSet<>(Arrays.asList('a','e','i','o','u'));
-        int[] ans=new int[queries.length];
-        int[] prefixSum=new int[words.length];
-        int sum=0;
-        for(int i=0;i<words.length;i++){
-            String currentWord=words[i];
-            if(set.contains(currentWord.charAt(0)) && set.contains(currentWord.charAt(currentWord.length()-1))){
-                sum++;
-            }
-            prefixSum[i]=sum;
+    public boolean isVowel(Character c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
         }
+        return false;
+    }
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int len=queries.length;
+        int[] ans=new int[len];
+        int[] vow=new int[words.length];
 
-        for(int i=0;i<queries.length;i++){
-            int[] currentQuery=queries[i];
-            ans[i]=prefixSum[currentQuery[1]]-(currentQuery[0] == 0 ? 0:prefixSum[currentQuery[0]-1]);
+        for(int i=0;i<words.length;i++){
+            String s=words[i];
+            if(isVowel(s.charAt(0)) && isVowel(s.charAt(s.length()-1))){
+              vow[i]=1;  
+            }
+            else{
+                vow[i]=0;
+            }
+        }
+        for(int i=1;i<vow.length;i++){
+            vow[i]+=vow[i-1];
+        }
+        for(int i=0;i<len;i++){
+            int start=queries[i][0];
+            int end=queries[i][1];
+            if(start == 0){
+                ans[i]=vow[end];
+            }
+            else{
+                ans[i]=vow[end]-vow[start-1];
+            }
         }
         return ans;
     }

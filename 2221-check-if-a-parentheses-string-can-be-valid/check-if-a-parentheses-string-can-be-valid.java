489c92ff2ee8295603f389b57c19df998ed1e299
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-12 12:40:09 +0530

    Time: 11 ms (91.70%) | Memory: 45.9 MB (37.76%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -1,34 +1,26 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
         if(s.length() % 2 == 1)return false;
-        int openBracket=0;
-        int unlocked=0;
+        int upper=0;
+        int lower=0;
         for(int i=0;i<s.length();i++){
-            if(locked.charAt(i) == '0')unlocked++;
-            else if(s.charAt(i) == '(')openBracket++;
-            else if(s.charAt(i) == ')'){
-                if(openBracket > 0)openBracket--;
-                else if(unlocked > 0) unlocked--;
-                else return false;
+            if(locked.charAt(i) == '1'){
+                if(s.charAt(i) == '('){
+                    upper++;
+                    lower++;
+                }
+                else{
+                    upper--;
+                    lower--;
+                }
             }
+            else{
+                upper++;
+                lower--;
+            }
+                if(lower < 0)lower+=2;
+                if(upper < 0)return false;
         }
-        int balance=0;
-        for(int i=s.length()-1;i>=0;i--){
-            if(locked.charAt(i) == '0'){
-                balance--;
-                unlocked--;
-            }
-            else if(s.charAt(i) == '('){
-                balance++;
-                openBracket--;
-            }
-            else if(s.charAt(i) == ')'){
-                balance--;
-            }
-            if(balance > 0)return false;
-            if(unlocked == 0 && openBracket == 0)break;
-        }
-        if(openBracket > 0) return false;
-        return true;
+        return lower == 0;
     }
 }

2625b020e41afd9654e4a032f5697e1431278475
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-14 20:55:14 +0530

    Time: 2 ms (95.83%) | Memory: 45.7 MB (40.89%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int[] freq=new int[A.length];
+        int[] freq=new int[A.length+1];
         int[] ans=new int[A.length];
         int count=0;
         for(int i=0;i<A.length;i++){
-            freq[A[i]-1]++;
-            freq[B[i]-1]++;
-            if(A[i]-1 == B[i]-1 && freq[A[i]-1] == 2)count++;
-            else{
-                if(freq[A[i]-1] == 2)count++;
-                if(freq[B[i]-1] == 2)count++;
-            }
+            if(++freq[A[i]] == 2)count++;
+            if(++freq[B[i]] == 2)count++;
             ans[i]=count;
         }
         return ans;

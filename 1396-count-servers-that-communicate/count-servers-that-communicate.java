26c5dbf6b7628b5a4856a0e29a3f7395c56ddaab
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-23 13:38:07 +0530

    Time: 3 ms (47.43%) | Memory: 52.7 MB (38.37%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean check(int[][] grid,int i,int j){
+        int count=0;
+        for(int k=0;k<grid.length;k++){
+            if(grid[k][j] == 1){
+                count++;
+            }
+            if(count >= 2)return false;
+        }
+        count=0;
+        for(int k=0;k<grid[0].length;k++){
+            if(grid[i][k] == 1)count++;
+            if(count >= 2) return false;
+        }
+        return true;
+    }
+    public int countServers(int[][] grid) {
+        int ans=0;
+        int countSet=0;
+        int countUnset=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    countSet++;
+                    if(check(grid,i,j)){
+                        countUnset++;
+                    }
+                }
+            }
+        }
+        return countSet-countUnset;
+    }
+}

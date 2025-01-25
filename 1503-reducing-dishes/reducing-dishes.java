3f5de2939286cd642da384ed952f398793543cc8
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-25 14:50:03 +0530

    Time: 2 ms (100.00%) | Memory: 41.5 MB (87.70%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -1,21 +1,17 @@
 class Solution {
-    public int helper(List<Integer> list,int ans,int max){
-        if(list.isEmpty())return max;
-        for(int i=1;i<=list.size();i++){
-            ans+=i*list.get(i-1);
-        }
-        list.remove(list.get(0));
-        max=Math.max(max,ans);
-        return helper(list,0,max);
-    }
     public int maxSatisfaction(int[] satisfaction) {
-        List<Integer> list=new ArrayList<>();
-        for(int n:satisfaction){
-            list.add(n);
+        Arrays.sort(satisfaction);
+        int maxSatisfaction=0;
+        int cumulativeSum=0;
+        for(int i=satisfaction.length-1;i>=0;i--){
+            if(cumulativeSum + satisfaction[i] > 0){
+                cumulativeSum+=satisfaction[i];
+                maxSatisfaction+=cumulativeSum;
+            }
+            else{
+                break;
+            }
         }
-        int ans=0;
-        int max=0;
-        Collections.sort(list);
-        return helper(list,ans,max);
+        return maxSatisfaction;
     }
 }

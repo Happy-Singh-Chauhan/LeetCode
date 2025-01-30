4dbb4a9fc1ec7819ac1acb03d2ff3ad8375c87b4
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-30 15:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (61.58%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.java
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -1,48 +1,14 @@
 class Solution {
+    public boolean check(String a,String b){
+        for(int i=0;i<a.length();i++){
+            if(a.indexOf(a.charAt(i)) != b.indexOf(b.charAt(i)))return false;
+        }
+        return true;
+    }
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> ans=new ArrayList<>();
-        if(words[0].equals("abcdefghijklab") && words[1].equals("abcdefghijkabl")){
-            ans.add("abcdefghijklab");
-            System.out.println(ans);
-            return ans;
-        }
-        if(pattern.length() == 1){
-            for(String s:words){
-                ans.add(s);
-            }
-            return ans;
-        }
-        Map<Character,Integer> map=new HashMap<>();
-        String s1="";
-        int a=1;
-        for(char c:pattern.toCharArray()){
-            if(!map.containsKey(c)){
-                s1+=a;
-                map.put(c,a);
-                a++;
-            }
-            else{
-                s1+=map.get(c);
-            }
-        }
-
-        for(String str:words){
-            Map<Character,Integer> map1=new HashMap<>();
-            String s2="";
-            int b=1;
-            for(char c:str.toCharArray()){
-                if(!map1.containsKey(c)){
-                    s2+=b;
-                    map1.put(c,b);
-                    b++;
-                }
-                else{
-                    s2+=map1.get(c);
-                }
-            }
-            if(s1.equals(s2)){
-                ans.add(str);
-            }
+        for(String word:words){
+            if(check(word,pattern))ans.add(word);
         }
         return ans;
     }

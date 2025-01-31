32a998493e2895dfafa8e0168a0c4d4267b6941d
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-01-31 14:25:59 +0530

    Time: 1 ms (74.32%) | Memory: 45.8 MB (29.49%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void transform(int[][] matrix,int i,int j){
+        for(int k=0;k<matrix[0].length;k++){
+            if(matrix[i][k] == 0)continue;
+            matrix[i][k]=-69;
+        }
+        for(int k=0;k<matrix.length;k++){
+            if(matrix[k][j] == 0)continue;
+            matrix[k][j]=-69;
+        }
+    }
+    public int[][] setZeroes(int[][] matrix) {
+        int count=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0)count++;
+            }
+        }
+        if(count == 0)return matrix;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    transform(matrix,i,j);
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j] == -69){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        return matrix;
+    }
+}

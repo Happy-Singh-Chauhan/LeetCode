895924d32259d8e5a74466613e06ecec6951f86f
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-02-19 14:57:09 +0530

    Time: 22 ms (29.84%) | Memory: 45.4 MB (21.87%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void generateHappyString(int n,String currentString,List<String> list){
+        if(currentString.length() == n){
+            list.add(currentString);
+            return;
+        }
+        for(char ch='a';ch<='c';ch++){
+            if(currentString.length() > 0 && currentString.charAt(currentString.length()-1) == ch)continue;
+            generateHappyString(n,currentString + ch,list);
+        }
+    }
+    public String getHappyString(int n, int k) {
+       List<String> list =new ArrayList<>();
+        String currentString="";
+        generateHappyString(n,currentString,list);
+
+        if(list.size() < k)return "";
+        return list.get(k-1);
+    }
+}

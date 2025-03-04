236723b1aa33ed01751a13f30ccdf8353c285db7
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-03-04 12:09:32 +0530

    Time: 9 ms (6.52%) | Memory: 43.7 MB (44.87%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,23 +1,20 @@
 class Solution {
-    public void generateSubsets(int index,List<List<Integer>> ans,int[] nums,Set<List<Integer>> set,List<Integer> list){
+    public void generateSubsets(int index,List<List<Integer>> ans,int[] nums,List<Integer> list){
         if(index == nums.length){
             if(!ans.contains(list)){
-            if(!set.contains(list)){
                 ans.add(new ArrayList<>(list));
-            }
             }
             return;
         }
         list.add(nums[index]);
-        generateSubsets(index+1,ans,nums,set,list);
+        generateSubsets(index+1,ans,nums,list);
         list.remove(list.size()-1);
-        generateSubsets(index+1,ans,nums,set,list);
+        generateSubsets(index+1,ans,nums,list);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        Set<List<Integer>> set=new HashSet<>();
         Arrays.sort(nums);
-        generateSubsets(0,ans,nums,set,new ArrayList<>());
+        generateSubsets(0,ans,nums,new ArrayList<>());
         return ans;
     }
 }

d457180309f8243d1b3d82dedaeb90b874865a65
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-03-05 11:47:38 +0530

    Time: 2 ms (39.97%) | Memory: 44.7 MB (31.15%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,22 +1,19 @@
 class Solution {
-    public void generate(int index,List<List<Integer>> ans,int[] nums,List<Integer> list){
-        if(index == nums.length){
+    public void generate(List<List<Integer>> ans,int[] nums,List<Integer> list){
             if(list.size() == nums.length){
                 ans.add(new ArrayList<>(list));
+                return;
             }
-            return;
-        }
         for(int i=0;i<nums.length;i++){
             if(list.contains(nums[i]))continue;
             list.add(nums[i]);
-            generate(index+1,ans,nums,list);
+            generate(ans,nums,list);
             list.remove(list.size()-1);
-            generate(index+1,ans,nums,list);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        generate(0,ans,nums,new ArrayList<>());
+        generate(ans,nums,new ArrayList<>());
         return ans;
     }
 }

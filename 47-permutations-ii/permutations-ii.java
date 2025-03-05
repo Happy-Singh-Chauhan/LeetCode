2ded3614ca57494c1cca7fd743dec1cc5169674f
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-03-05 12:18:41 +0530

    Time: 1 ms (99.84%) | Memory: 44.6 MB (93.56%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,28 +1,34 @@
 class Solution {
-    public void generate(List<List<Integer>> ans,int[] nums,List<Integer> list){
-            if(list.size() == nums.length){
-                if(!ans.contains(new ArrayList<>(list))){
-                List<Integer> list2=new ArrayList<>();
-                    for(int i=0;i<list.size();i++){
-                        list2.add(nums[list.get(i)]);
-                    }
-                    if(!ans.contains(list2)){
-                    ans.add(new ArrayList<>(list2));
-                    }
-                }
-                    return;
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public boolean isDuplicate(int[] nums,int start,int end){
+        for(int i=start;i<end;i++){
+            if(nums[i] == nums[end])return true;
+        }
+        return false;
+    }
+    public void generate(int index,List<List<Integer>> ans,int[]nums){
+        if(index == nums.length){
+            List<Integer> list=new ArrayList<>();
+            for(int num:nums){
+                list.add(num);
             }
-                
-        for(int i=0;i<nums.length;i++){
-            if(list.contains(i))continue;
-            list.add(i);
-            generate(ans,nums,list);
-            list.remove(list.size()-1);
+            ans.add(list);
+            return;
+        }
+        for(int i=index;i<nums.length;i++){
+            if(isDuplicate(nums,index,i))continue;
+            swap(nums,index,i);
+            generate(index+1,ans,nums);
+            swap(nums,index,i);
         }
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        generate(ans,nums,new ArrayList<>());
-        return ans;
+     List<List<Integer>> ans=new ArrayList<>();
+     generate(0,ans,nums);
+     return ans;   
     }
 }

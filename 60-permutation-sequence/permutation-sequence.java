8dc44b06c40f42bf0e986e26f22a975fefade153
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-01 23:29:27 +0530

    Time: 2 ms (51.94%) | Memory: 41.3 MB (42.15%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -8,14 +8,14 @@ class Solution {
       }  
       fact/=n;
       k-=1;
-      StringBuilder sb=new StringBuilder();
+      String ans="";
       while(true){
-        sb.append(list.get(k / fact));
+        ans+=list.get(k / fact);
         list.remove(k / fact);
         if(list.size() == 0)break;
         k%=fact;
         fact/=list.size();
       }
-      return String.valueOf(sb);
+      return ans;
     }
 }

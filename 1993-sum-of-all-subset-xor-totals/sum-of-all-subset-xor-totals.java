233528824794b9e78459fc3d83d9b96a5d66c233
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-05 12:35:26 +0530

    Time: 6 ms (21.45%) | Memory: 44.5 MB (13.62%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,26 +1,22 @@
 class Solution {
-    public void generateSubsets(List<List<Integer>> list,int index,int[] nums,List<Integer> list1){
+    int sum=0;
+    public void generateSubsets(int index,int[] nums,List<Integer> list){
         if(index == nums.length){
-            list.add(new ArrayList<>(list1));
-            return;
-        }
-        list1.add(nums[index]);
-        generateSubsets(list,index+1,nums,list1);
-        list1.remove(list1.size()-1);
-        generateSubsets(list,index+1,nums,list1);
-    }
-    public int subsetXORSum(int[] nums) {
-        List<List<Integer>> list=new ArrayList<>();
-        int index=0;
-        generateSubsets(list,index,nums,new ArrayList<>());
-        int sum=0;
-        for(List<Integer> lst:list){
             int xor=0;
-            for(int num:lst){
+            for(int num:list){
                 xor^=num;
             }
             sum+=xor;
+            return;
         }
+        list.add(nums[index]);
+        generateSubsets(index+1,nums,list);
+        list.remove(list.size()-1);
+        generateSubsets(index+1,nums,list);
+    }
+    public int subsetXORSum(int[] nums) {
+        int index=0;
+        generateSubsets(index,nums,new ArrayList<>());
         return sum;
     }
 }

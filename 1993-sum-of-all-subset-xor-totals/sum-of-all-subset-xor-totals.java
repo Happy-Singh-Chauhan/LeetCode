92ec2d3c2f0c3a247a90f85cec5c4839f6ef4530
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-05 12:38:32 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (63.91%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,22 +1,9 @@
 class Solution {
-    int sum=0;
-    public void generateSubsets(int index,int[] nums,List<Integer> list){
-        if(index == nums.length){
-            int xor=0;
-            for(int num:list){
-                xor^=num;
-            }
-            sum+=xor;
-            return;
-        }
-        list.add(nums[index]);
-        generateSubsets(index+1,nums,list);
-        list.remove(list.size()-1);
-        generateSubsets(index+1,nums,list);
-    }
     public int subsetXORSum(int[] nums) {
-        int index=0;
-        generateSubsets(index,nums,new ArrayList<>());
-        return sum;
+       int result=0;
+       for(int num:nums){
+        result|=num;
+       } 
+       return result << (nums.length-1);
     }
 }

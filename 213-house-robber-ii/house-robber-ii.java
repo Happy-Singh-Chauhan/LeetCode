5ba4380b6b8b0d13fb04faf660657f70447c8124
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-08 01:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (80.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -5,23 +5,31 @@ class Solution {
        int n=nums.length;
        int[] dp=new int[n];
        Arrays.fill(dp,-1);
-       dp[0]=nums[0];
+       int curr=0;
+       int prev=nums[0];
+       int prev2=0;
        for(int i=1;i<n-1;i++){
         int pick=nums[i];
-        if( i > 1)pick+=dp[i-2];
-        int skip=dp[i-1];
-        dp[i]=Math.max(pick,skip);
+        if( i > 1)pick+=prev2;
+        int skip=prev;
+        curr=Math.max(pick,skip);
+        prev2=prev;
+        prev=curr;
        }
-       int ans1=dp[n-2];
+       int ans1=prev;
        Arrays.fill(dp,-1);
-       dp[1]=nums[1];
+       prev=nums[1];
+       prev2=0;
+       curr=0;
        for(int i=2;i<n;i++){
         int pick=nums[i];
-        if(i > 2)pick+=dp[i-2];
-        int skip=dp[i-1];
-        dp[i]=Math.max(pick,skip);
+        if(i > 2)pick+=prev2;
+        int skip=prev;
+        curr=Math.max(pick,skip);
+        prev2=prev;
+        prev=curr;
        }
-       int ans2=dp[n-1];
+       int ans2=prev;
        return Math.max(ans1,ans2);
     }
 }

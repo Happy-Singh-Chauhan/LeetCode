c9f672217419717babdd91d16689a2245c24b8bb
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-08 01:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (27.42%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,37 +1,23 @@
 class Solution {
+    public int f(int start,int end,int[] nums){
+        int j=start;
+        int curr=0;
+        int prev=nums[start];
+        int prev2=0;
+        for(int i=start+1;i<=end;i++){
+            int pick=nums[i];
+            if(i > j+1)pick+=prev2;
+            int skip=prev;
+            curr=Math.max(pick,skip);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
     public int rob(int[] nums) {
-       int n=nums.length;
         if(nums.length == 1)return nums[0];
         if(nums.length == 2)return Math.max(nums[0],nums[1]);
-
-       int curr=0;
-       int prev=nums[0];
-       int prev2=0;
-
-       for(int i=1;i<n-1;i++){
-        int pick=nums[i];
-        if( i > 1)pick+=prev2;
-        int skip=prev;
-        curr=Math.max(pick,skip);
-        prev2=prev;
-        prev=curr;
-       }
-       int ans1=prev;
-
-       prev=nums[1];
-       prev2=0;
-       curr=0;
-
-       for(int i=2;i<n;i++){
-        int pick=nums[i];
-        if(i > 2)pick+=prev2;
-        int skip=prev;
-        curr=Math.max(pick,skip);
-        prev2=prev;
-        prev=curr;
-       }
-       int ans2=prev;
-
-       return Math.max(ans1,ans2);
+        int n=nums.length;
+        return Math.max(f(0,n-2,nums),f(1,n-1,nums));
     }
 }

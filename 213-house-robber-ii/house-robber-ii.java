fda6528356ac02e74fde5fac9158af6f40d0a285
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-08 01:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (64.36%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
+       int n=nums.length;
         if(nums.length == 1)return nums[0];
         if(nums.length == 2)return Math.max(nums[0],nums[1]);
-       
-       int n=nums.length;
-       int[] dp=new int[n];
-       Arrays.fill(dp,-1);
+
        int curr=0;
        int prev=nums[0];
        int prev2=0;
+
        for(int i=1;i<n-1;i++){
         int pick=nums[i];
         if( i > 1)pick+=prev2;
@@ -18,10 +17,11 @@ class Solution {
         prev=curr;
        }
        int ans1=prev;
-       Arrays.fill(dp,-1);
+
        prev=nums[1];
        prev2=0;
        curr=0;
+
        for(int i=2;i<n;i++){
         int pick=nums[i];
         if(i > 2)pick+=prev2;
@@ -31,6 +31,7 @@ class Solution {
         prev=curr;
        }
        int ans2=prev;
+
        return Math.max(ans1,ans2);
     }
 }

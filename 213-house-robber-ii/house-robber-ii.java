5edbb759d23a4bdfa2633238c06374cd4454e006
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-08 01:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (27.42%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1)return nums[0];
-        if(nums.length == 2)return Math.max(nums[0],nums[1]);
-        if(nums.length == 3)return Math.max(Math.max(nums[0],nums[1]),nums[2]);
+        
+       
        int n=nums.length;
        int[] dp=new int[n];
        Arrays.fill(dp,-1);

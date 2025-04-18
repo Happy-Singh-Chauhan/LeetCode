d9f67401ba67fbf2117f03e822ed69f0e52d727f
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-19 02:00:20 +0530

    Time: 8 ms (98.97%) | Memory: 57.6 MB (14.40%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n=nums.length;
-        int sum=0;
-        for(int num:nums)sum+=num;
-        if(sum % 2 == 1)return false;
-        int target=sum/2;
-        boolean[][] dp=new boolean[n][target+1];
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
+        int sum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            sum += num;
         }
-        dp[0][0]=true;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=target;j++){
-                boolean notTake=dp[i-1][j];
-                boolean take=false;
-                if(nums[i] <= j){
-                    take=dp[i-1][j-nums[i]];
-                }
-                dp[i][j]=take | notTake;
-            }
-        }
-        return dp[n-1][target];
+        // if sum is odd, it cannot be partitioned into two equal subgroups
+        if (sum % 2 != 0) return false;
+        int subsetSum = sum / 2;
+        int n = nums.length;
+        // basically a map with complex index / subsetSum key
+        Boolean[][] memo = new Boolean[n + 1][subsetSum + 1];
+        return dfs(nums, n - 1, subsetSum, memo);
+    }
+
+    public boolean dfs(int[] nums, int n, int subsetSum, Boolean[][] memo) {
+        if (subsetSum == 0)
+            return true;
+        if (n == 0 || subsetSum < 0)
+            return false;
+        if (memo[n][subsetSum] != null)
+            return memo[n][subsetSum];
+        boolean result = dfs(nums, n - 1, subsetSum - nums[n - 1], memo) ||
+                dfs(nums, n - 1, subsetSum, memo);
+        // store the result in memo
+        memo[n][subsetSum] = result;
+        return result;
     }
 }

7591e7466b283d8f7878eb90a1bfcf9be203cb94
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-19 10:59:50 +0530

    Time: 16 ms (96.07%) | Memory: 42.2 MB (72.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,30 +1,17 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        // find sum of all array elements
-        for (int num : nums) {
-            sum += num;
+        int total=0;
+        for(int num:nums)total+=num;
+        if(total % 2 == 1)return false;
+        int target=total/2;
+        boolean[] dp=new boolean[target + 1];
+        dp[0]=true;
+        for(int num:nums){
+            for(int j=target;j>=num;j--){
+                dp[j]=dp[j] | dp[j-num];
+            }
+            if(dp[target])return true;
         }
-        // if sum is odd, it cannot be partitioned into two equal subgroups
-        if (sum % 2 != 0) return false;
-        int subsetSum = sum / 2;
-        int n = nums.length;
-        // basically a map with complex index / subsetSum key
-        Boolean[][] memo = new Boolean[n + 1][subsetSum + 1];
-        return dfs(nums, n - 1, subsetSum, memo);
-    }
-
-    public boolean dfs(int[] nums, int n, int subsetSum, Boolean[][] memo) {
-        if (subsetSum == 0)
-            return true;
-        if (n == 0 || subsetSum < 0)
-            return false;
-        if (memo[n][subsetSum] != null)
-            return memo[n][subsetSum];
-        boolean result = dfs(nums, n - 1, subsetSum - nums[n - 1], memo) ||
-                dfs(nums, n - 1, subsetSum, memo);
-        // store the result in memo
-        memo[n][subsetSum] = result;
-        return result;
+        return dp[target];
     }
 }

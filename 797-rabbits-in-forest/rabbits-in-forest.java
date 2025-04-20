342b0e5367a3d7e88975d5658c158e93c9010959
Happy Singh Chauhan <141399971+Happy-Singh-Chauhan@users.noreply.github.com>
2025-04-20 19:33:57 +0530

    Time: 1 ms (93.98%) | Memory: 41.9 MB (97.26%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,18 +1,23 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        Map<Integer,Integer> map=new HashMap<>();
+        int n=answers.length;
+        int max=0;
+        for(int num:answers){
+            max=Math.max(max,num);
+        }
+        int[] freq=new int[max+1];
         int count=0;
-        for(int i=0;i<answers.length;i++){
+        for(int i=0;i<n;i++){
             if(answers[i] == 0){
                 count++;
                 continue;
             }
-            if(!map.containsKey(answers[i]) || map.get(answers[i]) == 0){
-                map.put(answers[i],answers[i]);
+            if(freq[answers[i]] == 0){
                 count+=answers[i]+1;
+                freq[answers[i]]=answers[i];
             }
             else{
-                map.put(answers[i],map.get(answers[i])-1);
+                freq[answers[i]]-=1;
             }
         }
         return count;

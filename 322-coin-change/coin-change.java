ac461818a939b27e80fd6b2dcf7048b729e84798
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-04-22 14:24:09 +0530

    Time: 16 ms (58.86%) | Memory: 44.8 MB (45.69%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] dp=new int[n][amount+1];
+        int[] prev=new int[amount+1];
+        int[] curr=new int[amount+1];
         for(int i=0;i<=amount;i++){
-            if(i % coins[0] == 0)dp[0][i]=i/coins[0];
-            else dp[0][i]=(int)1e8;
+            if(i % coins[0] == 0)prev[i]=i/coins[0];
+            else prev[i]=(int)1e8;
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<=amount;j++){
-                 int np=dp[i-1][j];
+                 int np=prev[j];
                  int p=Integer.MAX_VALUE;
                  if(coins[i] <= j){
-                p=1+dp[i][j-coins[i]];
+                p=1+curr[j-coins[i]];
                 }
-            dp[i][j]=Math.min(np,p);
+            curr[j]=Math.min(np,p);
+            }
+            for(int k=0;k<prev.length;k++){
+                prev[k]=curr[k];
             }
         }
-        int ans=dp[n-1][amount];
+        int ans=prev[amount];
         if(ans >= 1e8)return -1;
         return ans;
     }

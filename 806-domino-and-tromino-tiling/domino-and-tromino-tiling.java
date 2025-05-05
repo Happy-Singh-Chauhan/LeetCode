badea2612d2d678c72223e83d571e6de276560c9
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-05-05 20:36:30 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (14.11%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numTilings(int n) {
+        int mod=1000000007;
+        if(n <= 1)return 1;
+        if(n == 2)return 2;
+        if(n == 3)return 5;
+        long[] dp=new long[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int i=4;i<=n;i++){
+            dp[i]=(2 * dp[i-1] % mod + dp[i-3]) % mod;
+        }
+        return (int)dp[n];
+    }
+}

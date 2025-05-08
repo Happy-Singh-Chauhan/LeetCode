0f7f0d5a68d897bcf4b2a52ede0ea35ac26f5b65
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-05-08 12:41:06 +0530

    Time: 19 ms (64.51%) | Memory: 51.1 MB (68.07%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,9 +1,9 @@
 class Solution {
     public int f(int i,int j,String s,String t,int[][] dp){
-        if(j < 0)return 1;
-        if(i < 0)return 0;
+        if(j == 0)return 1;
+        if(i == 0)return 0;
         if(dp[i][j] != -1)return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)){
+        if(s.charAt(i-1) == t.charAt(j-1)){
             return dp[i][j]=f(i-1,j,s,t,dp)+f(i-1,j-1,s,t,dp);
         }
         return dp[i][j]=f(i-1,j,s,t,dp);
@@ -11,8 +11,20 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
-        int[][] dp=new int[n][m];
-        for(int[] row:dp)Arrays.fill(row,-1);
-        return f(n-1,m-1,s,t,dp);
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 }

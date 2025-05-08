a0eb84ca6e6cb37e807cc3a20e84deddd295d8a7
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-05-08 12:41:44 +0530

    Time: 18 ms (80.82%) | Memory: 51.3 MB (57.20%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,13 +1,4 @@
 class Solution {
-    public int f(int i,int j,String s,String t,int[][] dp){
-        if(j == 0)return 1;
-        if(i == 0)return 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        if(s.charAt(i-1) == t.charAt(j-1)){
-            return dp[i][j]=f(i-1,j,s,t,dp)+f(i-1,j-1,s,t,dp);
-        }
-        return dp[i][j]=f(i-1,j,s,t,dp);
-    }
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();

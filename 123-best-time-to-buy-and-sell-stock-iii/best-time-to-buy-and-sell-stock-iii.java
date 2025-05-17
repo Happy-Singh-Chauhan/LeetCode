a10f3716e98a3edc471d8d9500a19910b7d54ad6
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-05-17 21:37:36 +0530

    Time: 10 ms (52.03%) | Memory: 61.2 MB (61.61%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,45 +1,26 @@
 class Solution {
-    public int f(int[] prices,int buy,int allowed,int index,int[][][] dp){
-        if(index == prices.length)return 0;
-        if(allowed == 0)return 0;
-        if(dp[index][buy][allowed] != -1)return dp[index][buy][allowed];
-        int maxProfit=0;
-        if(buy == 1){
-            maxProfit=Math.max(-prices[index]+f(prices,0,allowed,index+1,dp),f(prices,1,allowed,index+1,dp));
-        }
-        else{
-            maxProfit=Math.max(prices[index]+f(prices,1,allowed-1,index+1,dp),f(prices,0,allowed,index+1,dp));
-        }
-        return dp[index][buy][allowed]=maxProfit;
-    }
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int[][][] dp=new int[n+1][2][3];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=1;j++){
-                dp[i][j][0]=0;
-            }
-        }
-        for(int i=0;i<=1;i++){
-            for(int j=0;j<=2;j++){
-                dp[n][i][j]=0;
-            }
-        }
+        int[][] after=new int[2][3];
+        int[][] curr=new int[2][3];
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<=1;j++){
-                for(int k=0;k<=2;k++){
+                for(int k=1;k<=2;k++){
                     int maxProfit=0;
                     if(j == 1){
-            dp[i][j][k]=Math.max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
-        }
-        else{
-            if(k != 0){
-            dp[i][j][k]=Math.max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                        curr[j][k]=Math.max(-prices[i]+after[0][k],after[1][k]);
+                    }
+                    else{
+                        curr[j][k]=Math.max(prices[i]+after[1][k-1],after[0][k]);
+                    }
+                }
             }
-        }
+            for(int l=0;l<=1;l++){
+                for(int m=0;m<=2;m++){
+                    after[l][m]=curr[l][m];
                 }
             }
         }
-       return dp[0][1][2]; 
+        return after[1][2];
     }
 }

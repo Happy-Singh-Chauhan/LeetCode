7ded4cfcb08328650c477f64e2acb7076f3e30af
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-05-21 23:59:27 +0530

    Time: 41 ms (58.76%) | Memory: 44.2 MB (77.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,15 +3,15 @@ class Solution {
         int n=nums.length;
         int[] dp=new int[n];
         Arrays.fill(dp,1);
+        int max=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[j] < nums[i]){
                 dp[i]=Math.max(1+dp[j],dp[i]);
                 }
             }
+            max=Math.max(max,dp[i]); 
         }
-        int max=0;
-        for(int num:dp)max=Math.max(max,num);
         return max;
     }
 }

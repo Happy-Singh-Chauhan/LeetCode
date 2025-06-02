066690272fe08788f86e6534cb9661ec9e27dc93
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-06-02 21:36:50 +0530

    Time: 3 ms (84.79%) | Memory: 46.2 MB (16.41%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,30 +1,16 @@
 class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int[] arr1=new int[n];
-        int[] arr2=new int[n];
-        arr1[0]=1;
+        int[] arr=new int[n];
+        Arrays.fill(arr,1);
         for(int i=1;i<n;i++){
-            if(ratings[i] < ratings[i-1]){
-                arr1[i]=1;
-               if(arr1[i-1] == 1)arr1[i-1]=arr1[i]+1; 
-            }
-            else if(ratings[i] == ratings[i-1])arr1[i]=1;
-            else arr1[i]=arr1[i-1]+1;
+            if(ratings[i] > ratings[i-1])arr[i]=arr[i-1]+1;
         }
-        arr2[n-1]=1;
         for(int i=n-2;i>=0;i--){
-            if(ratings[i] < ratings[i+1]){
-                arr2[i]=1;
-                if(arr2[i+1] == 1)arr2[i+1]=arr2[i]+1;
-            }
-            else if(ratings[i] == ratings[i+1])arr2[i]=1;
-            else arr2[i]=arr2[i+1]+1;
+            if(ratings[i] > ratings[i+1])arr[i]=Math.max(arr[i],arr[i+1]+1);
         }
         int count=0;
-        for(int i=0;i<n;i++){
-            count+=Math.max(arr1[i],arr2[i]);
-        }
+        for(int num:arr)count+=num;
         return count;
     }
 }

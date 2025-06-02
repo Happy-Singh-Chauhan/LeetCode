05517b54ecd64c63d469510068983622ee00d357
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-06-02 20:29:15 +0530

    Time: 24 ms (31.43%) | Memory: 41.2 MB (32.86%) - LeetSync

---
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        long ans=0;
+        for(int i=0;i<=Math.min(limit,n);i++){
+            if(n-i > 2*limit)continue;
+            ans+=Math.min(limit,n-i)-Math.max(0,n-i-limit)+1;
+        }
+        return ans;
+    }
+}

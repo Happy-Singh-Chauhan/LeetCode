c3201cfb5a8b7de9fc5cf96babe0c1f1a12ec904
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-06-07 12:35:21 +0530

    Time: 15 ms (68.62%) | Memory: 46.7 MB (47.43%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,44 +1,42 @@
 class Solution {
-     private int getMax(int[] arr,Stack<Integer> stack,int max,int i){
+    public int getArea(Stack<Integer> stack,int[] heights,int max,int i){
         int area;
         int popped=stack.pop();
         if(stack.isEmpty()){
-            area=arr[popped]*i;
+            area=heights[popped]*i;
         }
-        else{
-            area=arr[popped]*(i-1-stack.peek());
-        }
-        return Math.max(max,area);
+        else area=heights[popped]*(i-1-stack.peek());
+        return Math.max(max,area); 
     }
-    public int largestRectangleArea(int[] heights) {
+    public int getMaxArea(int[] heights){
+        int n=heights.length;
         Stack<Integer> stack=new Stack<>();
-        int max=0;
         stack.push(0);
-        for(int i=1;i<heights.length;i++){
-            while(!stack.isEmpty() && heights[i]<heights[stack.peek()]){
-                max=getMax(heights,stack,max,i);
-            } 
+        int max=0;
+        for(int i=1;i<n;i++){
+            while(!stack.isEmpty() && heights[i] < heights[stack.peek()]){
+                max=getArea(stack,heights,max,i);
+            }
             stack.push(i);
         }
-        int i=heights.length;
         while(!stack.isEmpty()){
-            max=getMax(heights,stack,max,i);
+            max=getArea(stack,heights,max,n);
         }
         return max;
     }
     public int maximalRectangle(char[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-        int[] area=new int[m];
-        int max=0;
+        int[] arr=new int[m];
+        int maxArea=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j] == '1')area[j]+=1;
-                else area[j]=0;
+                if(matrix[i][j] == '1')arr[j]+=1;
+                else arr[j]=0;
             }
-            int maxArea=largestRectangleArea(area);
-            max=Math.max(max,maxArea);
+            int area=getMaxArea(arr);
+            maxArea=Math.max(maxArea,area);
         }
-        return max;
+        return maxArea;
     }
 }

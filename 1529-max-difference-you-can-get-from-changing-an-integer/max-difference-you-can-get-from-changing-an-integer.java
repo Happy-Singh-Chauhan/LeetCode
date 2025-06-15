0499a39c6bad2f86a777f61bdb92fdc8be339394
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-06-15 11:12:58 +0530

    Time: 2 ms (31.43%) | Memory: 41.3 MB (22.86%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -1,21 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int maxDiff(int num) {
-        String s=""+num;
-        char c=s.charAt(0);
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        for(char ch:s.toCharArray()){
-            String str1=s.replace(ch,'9');
-            max=Math.max(max,Integer.parseInt(str1));
-            if(ch != c){
-                String str2=s.replace(ch,'0');
-                min=Math.min(min,Integer.parseInt(str2));
-            }
-            else{
-                String str2=s.replace(ch,'1');
-                min=Math.min(min,Integer.parseInt(str2));
-            }
+        String s = Integer.toString(num);
+        Set<Character> seen = new HashSet<>();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (char ch : s.toCharArray()) {
+            if (seen.contains(ch)) continue;
+            seen.add(ch);
+
+            // Replace with '9' for maximum
+            String maxStr = s.replace(ch, '9');
+            max = Math.max(max, Integer.parseInt(maxStr));
+
+            // Replace with '1' for first digit, '0' otherwise (to avoid leading zero)
+            char replaceWith = (ch == s.charAt(0)) ? '1' : '0';
+            // Skip if it would create a leading zero
+            if (replaceWith == '0' && ch == s.charAt(0)) continue;
+
+            String minStr = s.replace(ch, replaceWith);
+            min = Math.min(min, Integer.parseInt(minStr));
         }
-        return max-min;
+
+        return max - min;
     }
 }

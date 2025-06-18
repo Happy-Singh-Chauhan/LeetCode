45265160c84ecc40ce46c539e2e236e63a4a802d
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-06-18 20:04:33 +0530

    Time: 22 ms (94.76%) | Memory: 59.8 MB (42.93%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -5,12 +5,10 @@ class Solution {
         int part=n/3;
         int[][] arr=new int[part][3];
         int p=0;
-        while(p < n){
-            for(int j=0;j<part;j++){
-                for(int l=0;l<3;l++){
-                    arr[j][l]=nums[p];
-                    p++;
-                }
+        for(int j=0;j<part;j++){
+            for(int l=0;l<3;l++){
+                arr[j][l]=nums[p];
+                p++;
             }
         }
         for(int i=0;i<arr.length;i++){

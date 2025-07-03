5c18c0e70657ddf1e00f5107bc9d8977064f8191
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-07-03 13:20:51 +0530

    Time: 3 ms (74.89%) | Memory: 42.9 MB (46.75%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,6 +1,18 @@
 class Solution {
+    public char helper(String s,int k){
+        if(s.length() >= k)return s.charAt(k-1);
+        StringBuilder sb=new StringBuilder(s);
+        int index=0;
+        while(index < s.length()){
+            char ch=s.charAt(index);
+            if(ch == 'z')sb.append('a');
+            else sb.append((char)(ch + 1));
+            index++;
+        }
+        return helper(sb.toString(),k);
+    }
     public char kthCharacter(int k) {
-        String s="abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij";
-    return s.charAt(k-1);              
+        String s="a";
+        return helper(s,k);
     }
 }

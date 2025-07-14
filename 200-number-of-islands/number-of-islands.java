5c8e87897cda6163b17d3bc9e573bb224b0fdb75
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-07-14 14:28:51 +0530

    Time: 3 ms (83.38%) | Memory: 49.5 MB (72.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,49 +1,26 @@
-class Pair{
-    int first;
-    int second;
-    public Pair(int first,int second){
-        this.first=first;
-        this.second=second;
-    }
-}
-
 class Solution {
-    public void bfs(int ro,int co,char[][] grid){
-        Queue<Pair> queue=new LinkedList<Pair>();
-        queue.add(new Pair(ro,co));
-        int n=grid.length;
-        int m=grid[0].length;
+    public void dfs(int row,int col,char[][] grid){
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0')return;
+        grid[row][col]='0';
 
-        while(!queue.isEmpty()){
-            int row=queue.peek().first;
-            int col=queue.peek().second;
-            queue.remove();
-            int[] delrow={-1,0,1,0};
-            int[] delcol={0,1,0,-1};
-            for(int k=0;k<4;k++){
-                int nrow=row+delrow[k];
-                int ncol=col+delcol[k];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1'){
-                    grid[nrow][ncol]='0';
-                    queue.add(new Pair(nrow,ncol));
-                    }
-                }
-            }
+        dfs(row,col-1,grid);
+        dfs(row-1,col,grid);
+        dfs(row,col+1,grid);
+        dfs(row+1,col,grid);
         return;
     }
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1'){
-                    count++;
-                    bfs(i,j,grid);
-                }
+       int n=grid.length;
+       int m=grid[0].length;
+       int count=0;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j] == '1'){
+                count++;
+                dfs(i,j,grid);
             }
         }
-        return count;
+       } 
+       return count;
     }
 }

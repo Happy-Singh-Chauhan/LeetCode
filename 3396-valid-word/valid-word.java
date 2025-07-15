7caa5440aa5f5939e24a6d0e27aa75859c976f5f
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-07-15 15:55:20 +0530

    Time: 2 ms (30.46%) | Memory: 42.6 MB (15.23%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isValid(String word) {
         if(word.length() < 3)return false;
-        int numberCount=0;
         int vowelCount=0;
         int consonantCount=0;
         String number="0123456789";
         String vowel="aeiouAEIOU";
         String consonant="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
         for(char c:word.toCharArray()){
-            if(number.contains(""+c))numberCount++;
+            if(number.contains(""+c))continue;
             else if(vowel.contains(""+c))vowelCount++;
             else if(consonant.contains(""+c))consonantCount++;
             else return false;

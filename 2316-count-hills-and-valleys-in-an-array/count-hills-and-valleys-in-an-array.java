bcd65471a3f7e60bece04b31d3edcdbc4992be62
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-07-27 12:54:54 +0530

    Time: 1 ms (33.40%) | Memory: 41.7 MB (54.10%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -6,26 +6,22 @@ class Solution {
             if(nums[i] == nums[i-1])continue;
             int j=i-1;
             int k=i+1;
-            int temp1=0;
-            int temp2=0;
             while(j >= 0){
                 if(nums[i] != nums[j]){
-                    temp1=nums[j];
                     break;
                 }
                 j--;
             }
             while(k < n){
                 if(nums[i] != nums[k]){
-                    temp2=nums[k];
                     break;
                 }
                 k++;
             }
-            if(temp1 == 0 || temp2 == 0)continue;
-            int[] arr=new int[2];
-            if(temp1 < nums[i] && temp2 < nums[i] || temp1 > nums[i] && temp2 > nums[i]){
+            if(j >= 0 && k < n ){
+            if( nums[i] < nums[j] && nums[i] < nums[k] || nums[i] > nums[j] && nums[i] > nums[k]){
             ans++;
+            }
             }
         }
         return ans;

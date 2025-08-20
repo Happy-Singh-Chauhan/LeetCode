a98984dc22676cef466da9f0a70a3714bf296533
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-08-20 08:49:36 +0530

    Time: 5 ms (98.48%) | Memory: 55.6 MB (29.78%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,25 +1,25 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-      int n=matrix.length;
-      int m=matrix[0].length;
-      int[][] dp=new int[n][m];
-      for(int i=0;i<n;i++){
-        dp[i][0]=matrix[i][0];
-      }  
-      for(int j=1;j<m;j++){
-        dp[0][j]=matrix[0][j];
-      }
-      for(int i=1;i<n;i++){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            dp[i][0]=matrix[i][0];
+        }
         for(int j=1;j<m;j++){
-            if(matrix[i][j] == 1){
-                dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]))+1;
+            dp[0][j]=matrix[0][j];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j] == 1){
+                dp[i][j]=Math.min(dp[i][j-1],Math.min(dp[i-1][j-1],dp[i-1][j]))+1;
+                }
             }
         }
-      }
-      int count=0;
-      for(int[] row:dp){
-        for(int num:row)count+=num;
-      }
-      return count;
+        int count=0;
+        for(int[] row:dp){
+            for(int num:row)count+=num;
+        }
+        return count;
     }
 }

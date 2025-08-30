d1495a4924c480f58b332aa2af13bc62768f28b9
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2025-08-30 12:04:44 +0530

    Time: 1 ms (99.87%) | Memory: 44.5 MB (66.47%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -7,13 +7,10 @@ class Solution {
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char c=board[i][j];
-                if(c == '.') continue;
+                if(c == '.')continue;
                 int num=c-'0'-1;
                 int k=i/3*3+j/3;
-
-                if(row[i][num] || col[j][num] || box[k][num]){
-                    return false;
-                }
+                if(row[i][num] || col[j][num] || box[k][num])return false;
                 row[i][num]=true;
                 col[j][num]=true;
                 box[k][num]=true;

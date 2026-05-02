1e56b86bdf96c4b80f8437e60d246fefb1c3817c
Happy Singh Chauhan <happysinghchauhan72@gmail.com>
2026-05-02 22:51:28 +0530

    Time: 4 ms (59.28%) | Memory: 42.5 MB (25.79%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,15 +1,31 @@
 class Solution {
-    public boolean check(int n){
-        String str=""+n;
-        if(str.contains("3") || str.contains("4") || str.contains("7"))return false;
-        if(str.contains("2") || str.contains("5") || str.contains("6") || str.contains("9"))return true;
-        return false;
-    }
     public int rotatedDigits(int n) {
-        int cnt=0;
-        for(int i=2;i<=n;i++){
-            if(check(i))cnt++;
+       int[] dp=new int[n+1];
+       int count=0;
+       for(int i=0;i<=n;i++){
+        if(i < 10){
+            if(i == 1 || i == 8 || i == 0){
+                dp[i]=1;
+            }
+            else if(i == 2 || i == 5 || i == 6 || i == 9){
+                dp[i]=2;
+                count++;
+            }
+            else dp[i]=0;
         }
-        return cnt;
+        else{
+            int a=dp[i/10];
+            int b=dp[i%10];
+            if(a == 1 && b == 1){
+                dp[i]=1;
+            }
+            else if(a >=1 && b >=1){
+                dp[i]=2;
+                count++;
+            }
+            else dp[i]=0;
+        }
+       } 
+       return count;
     }
 }
